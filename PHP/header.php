7eef37136aedc39ed2afea9485f100fa700e6eb8
Josh-Jilek <42814537+Josh-Jilek@users.noreply.github.com>
2018-10-19 12:48:31 -0500

    Add files via upload

---
+++ PHP/header.php
@@ -0,0 +1,5 @@
+<nav>
+            <a href="index.php">Page 1</a> 
+            <a href="page2.php">Page 2</a>
+            <a href="page3.php">Page 3</a>
+        </nav>
---
+++ PHP/page2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP Lab</title>
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+    <?php include 'header.php'; ?> 
+    <?php include 'vars.php'; ?>
+    <main>
+       
+        <?php include 'vars.php'; echo "I have a $color $car."; ?>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure, quod deserunt quasi corrupti voluptas magnam qui hic inventore tempore, velit ad sed, ex nihil. Quos fugit modi consectetur ut iure!</p>
+</main>
+
+    <?php include 'footer.php'; ?>
+
+</body>
+</html>
