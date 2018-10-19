2d68ee8c9a6ca118bc98cbc5bc4376436bee2cac
Josh-Jilek <42814537+Josh-Jilek@users.noreply.github.com>
2018-10-19 11:55:53 -0500

    Delete script.js

--- js/script.js
+++
@@ -1,7 +0,0 @@
-function change1(){
-    document.getElementById("large").src = "http://via.placeholder.com/325x325?text=1"
-}
-
-function change2(){
-    document.getElementById("large").src = "http://via.placeholder.com/325x325?text=2"
-}
