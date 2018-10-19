ea4d9a9fa9b1d02a8268b7a0569f96119f5c4584
Josh-Jilek <42814537+Josh-Jilek@users.noreply.github.com>
2018-10-19 11:54:14 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function change1(){
+    document.getElementById("large").src = "http://via.placeholder.com/325x325?text=1"
+}
+
+function change2(){
+    document.getElementById("large").src = "http://via.placeholder.com/325x325?text=2"
+}
